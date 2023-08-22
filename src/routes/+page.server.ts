523462104c7f487d5472902c1e01773adf545518
Dane Miller <khadane.miller@gmail.com>
2023-08-22 17:20:00 -0400

    working on fetching feed with following posts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,39 @@
 import { prisma_client } from '$lib/prisma';
 import { error } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
 
-export async function load() {
+export const load: PageServerLoad = async ({locals}) => {
+  const session = await locals.auth?.validate();
+  const user = session?.user;
+
+  if (!user) {
+
 	const firehose = await prisma_client.post.findMany({
 		take: 10,
 		include: { author: true },
 		orderBy: { createdAt: 'desc' }
 	});
+    return { firehose }
+  }
 
-	return { firehose };
+  const firehose = await prisma_client.user.findUnique({
+    where: {
+      id: user?.userId,
+    },
+    select: {
+      posts: {
+        include: {
+          author: true
+        }
+      },
+    },
+  })
+
+
+	return { firehose: firehose?.posts };
 }
 
-export const actions = {
+export const actions: Actions = {
 	createPost: async ({ locals, request }) => {
 		const form = await request.formData();
 		const content = form.get('content') as string;
