c604542641c2bd1cb76d1a623a27a319351a5b0c
Zeu Capua <zeucapua@gmail.com>
2023-08-28 13:44:23 -0700

    exclude user from following list, refactor prisma calls

--- src/routes/following/+page.server.ts
+++ src/routes/following/+page.server.ts
@@ -1,56 +1,47 @@
 import type { PageServerLoad } from './$types';
 import { prisma_client } from '$lib/prisma';
-import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.auth?.validate();
 	const user = session?.user;
-	const firehoseWithFollowers = await prisma_client.user.findUnique({
-		where: {
-			id: user?.userId
-		},
+
+	console.log({ user });
+
+	const user_data = await prisma_client.user.findUnique({
+		where: { id: user.userId },
 		select: {
 			following: {
 				select: {
-					following: {
-						select: {
-							posts: {
-								include: {
-									author: true
-								},
-								take: 10,
-								orderBy: { createdAt: 'desc' }
-							}
-						}
-					}
-				},
-				where: {
-					followerId: {
-						equals: user?.userId
-					}
+					followingId: true
 				}
-			},
-			posts: {
-				include: {
-					author: true
-				},
-				take: 10,
-				orderBy: { createdAt: 'desc' }
 			}
 		}
 	});
 
-	if (!firehoseWithFollowers) {
-		throw error(404, 'No posts found');
+	if (user_data?.following) {
+		const following_ids: string[] = [];
+		user_data.following.forEach((element) => {
+			following_ids.push(element.followingId);
+		});
+
+		const following_firehose = await prisma_client.post.findMany({
+			where: {
+				author: { id: { in: following_ids } }
+			},
+			include: { author: true },
+			orderBy: { createdAt: 'desc' }
+		});
+
+		/*
+		const following_firehose: { author: User; post: Post }[] = [];
+		following_data.forEach((element) => {
+			const { author, ...post } = element;
+			following_firehose.push({ author, post });
+		});
+    */
+
+		return { following_firehose };
 	}
 
-	// not sure how to sort the two different results
-	// right now whatever is first in the return array will be first
-	// but that's not right...
-	// is there a way in prisma to return the users posts and the follwers posts in the same array?
-	// instead of having to merge them after?
-	const usersPosts = firehoseWithFollowers.posts;
-	const followersPosts = firehoseWithFollowers?.following[0].following.posts;
-
-	return { firehose: [...usersPosts, ...followersPosts] };
+	return { following_firehose: null };
 };
--- src/routes/following/+page.svelte
+++ src/routes/following/+page.svelte
@@ -5,8 +5,10 @@
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
-	const firehose = data?.firehose;
 	const user = $page.data.user;
+	const following_firehose = data.following_firehose;
+
+	console.log(typeof following_firehose);
 </script>
 
 <h3 class="text-2xl">Home</h3>
@@ -15,7 +17,9 @@
 {/if}
 
 <div class="flex flex-col w-full max-w-2xl mx-auto gap-2">
-	{#each firehose as firepost}
-		<PostCard post={firepost} author={firepost.author} />
-	{/each}
+	{#if following_firehose}
+		{#each following_firehose as firepost}
+			<PostCard post={firepost} author={firepost.author} />
+		{/each}
+	{/if}
 </div>
